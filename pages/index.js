48ce5f566617320b0133bb64fe2305607bbcef81
yi.wu <yi.wu@shopee.com>
2023-11-09 10:21:33 +0800

    complete demo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import {getSortedPostsData} from "../lib/posts";
-
+import Link from "next/link";
+import Date from '../components/date'
 export default function Home({ allPostsData }) {
     return (
         <Layout home>
@@ -21,15 +22,16 @@ export default function Home({ allPostsData }) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>{title}</Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
             </section>
+
         </Layout>
     );
 }
