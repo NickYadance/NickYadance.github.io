371214b8764dbbd7e9c9494989d197c5a32238da
yi.wu <yi.wu@shopee.com>
2024-04-11 18:09:05 +0800

    dp

--- posts/codes/dp/dp_matrix_test.go
+++ posts/codes/dp/dp_matrix_test.go
@@ -306,3 +306,108 @@ func minimumTotalReverse(triangle [][]int) int {
 	}
 	return dp[0]
 }
+
+/*
+给你一个 n x n 的 方形 整数数组 matrix ，请你找出并返回通过 matrix 的下降路径 的 最小和 。
+
+下降路径 可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列（即位于正下方或者沿对角线向左或者向右的第一个元素）。具体来说，位置 (row, col) 的下一个元素应当是 (row + 1, col - 1)、(row + 1, col) 或者 (row + 1, col + 1) 。
+
+示例 1：
+
+输入：matrix = [[2,1,3],[6,5,4],[7,8,9]]
+输出：13
+解释：如图所示，为和最小的两条下降路径
+示例 2：
+
+输入：matrix = [[-19,57],[-40,-5]]
+输出：-59
+解释：如图所示，为和最小的下降路径
+
+提示：
+
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+-100 <= matrix[i][j] <= 100
+*/
+func minFallingPathSum(matrix [][]int) int {
+	/*
+		dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i-1][j+1]) + matrix[i][j]
+		dp[i] = min(x + dp[i] + dp[i+1]) + matrix[i][j]
+	*/
+	n := len(matrix)
+	dp := make([]int, n+1)
+	dp[0] = math.MaxInt
+	for i := 1; i <= n; i++ {
+		x := dp[0]
+		for j := 1; j <= n; j++ {
+			tmp := dp[j]
+			if j < n {
+				dp[j] = min(x, dp[j], dp[j+1]) + matrix[i-1][j-1]
+			} else {
+				dp[j] = min(x, dp[j]) + matrix[i-1][j-1]
+			}
+			x = tmp
+		}
+	}
+	ans := math.MaxInt
+	for i := 0; i <= n; i++ {
+		ans = min(ans, dp[i])
+	}
+	return ans
+}
+
+/*
+在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+
+示例 1：
+
+输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+输出：4
+示例 2：
+
+输入：matrix = [["0","1"],["1","0"]]
+输出：1
+示例 3：
+
+输入：matrix = [["0"]]
+输出：0
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] 为 '0' 或 '1'
+*/
+func maximalSquare(matrix [][]byte) int {
+	/*
+		let G(x) = x > 0 ? 1 : 0
+		dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) * G(matrix[i][j]) + G(matrix[i][j])
+				 = G(matrix[i][j])(1 + min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]))
+	*/
+	m := len(matrix)
+	n := len(matrix[0])
+	dp := make([]int, n+1)
+	ans := 0
+	G := func(i, j int) int {
+		if matrix[i][j] == '1' {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	for i := 1; i <= m; i++ {
+		x := 0
+		for j := 1; j <= n; j++ {
+			tmp := dp[j]
+			dp[j] = G(i-1, j-1) * (1 + min(x, dp[j-1], dp[j]))
+			x = tmp
+
+			if dp[j] > ans {
+				ans = dp[j]
+			}
+		}
+	}
+	return ans * ans
+}
